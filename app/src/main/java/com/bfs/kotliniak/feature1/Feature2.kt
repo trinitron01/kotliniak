c7954bd361b1c713b7432a55e4238e2646725fed
Bartłomiej Sory <bartlomiej.sory@intive.com>
2021-03-25 14:17:44 +0100

    master change for conflict 3

--- app/src/main/java/com/bfs/kotliniak/feature1/Feature2.kt
+++ app/src/main/java/com/bfs/kotliniak/feature1/Feature2.kt
@@ -3,6 +3,12 @@ package com.bfs.kotliniak.feature1
 class Feature2 {
 
     fun test() {
+            val masterChange  = 1
+    }
 
+
+
+    fun masterChangeFun() {
+        val masterChange  = 1
     }
 }
