5459a76a53a3fb1b485deaf45fb4f94f0d115bd6
Bartłomiej Sory <bartlomiej.sory@intive.com>
2021-03-25 14:38:45 +0100

    feature 4 commit 3

--- app/src/main/java/com/bfs/kotliniak/feature1/Feature4.kt
+++ app/src/main/java/com/bfs/kotliniak/feature1/Feature4.kt
@@ -11,4 +11,14 @@ class Feature4 {
     val a = 3
 
     val bAfterMerge = 8
+
+    fun tes3t() {
+
+        val other = 3
+        val chage1 = 2
+        val ot111her = 3
+        val ch111a23ge1 = 2
+        val ot1131her = 3
+        val ch111age1 = 2
+    }
 }
