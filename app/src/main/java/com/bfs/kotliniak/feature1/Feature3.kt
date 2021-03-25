38bee7b2d3206e39f4fd40d8e70f8076d1d1956e
Bartłomiej Sory <bartlomiej.sory@intive.com>
2021-03-25 14:49:30 +0100

    change on master after feature 5

--- app/src/main/java/com/bfs/kotliniak/feature1/Feature3.kt
+++ app/src/main/java/com/bfs/kotliniak/feature1/Feature3.kt
@@ -10,4 +10,16 @@ class Feature3 {
 
     }
 
+
+
+
+    fun tes34t() {
+
+        val other = 3
+        val chage1 = 2
+        val ot111her = 3
+        val ch111a23ge1 = 2
+        val ot1131her = 3
+        val ch111age1 = 2
+    }
 }
