931f5c2e2b06475238728d6e18961f59b14f30c7
Bartłomiej Sory <bartlomiej.sory@intive.com>
2021-03-26 11:38:45 +0100

    master change 5

--- app/src/main/java/com/bfs/kotliniak/feature1/Feature22.kt
+++ app/src/main/java/com/bfs/kotliniak/feature1/Feature22.kt
@@ -13,4 +13,14 @@ class Feature22 {
         val ch111a23ge1 = 2
 
     }
+
+
+    fun tes34342342t() {
+
+        val other = 3
+        val chage1 = 2
+        val ot111her = 3
+        val ch111a23ge1 = 2
+
+    }
 }
