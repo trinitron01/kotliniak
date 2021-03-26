22808737ea0126541a68aff2f092f3a705492e9c
Bartłomiej Sory <bartlomiej.sory@intive.com>
2021-03-26 11:25:34 +0100

    feature 2 commit 3

--- app/src/main/java/com/bfs/kotliniak/feature1/Feature2.kt
+++ app/src/main/java/com/bfs/kotliniak/feature1/Feature2.kt
@@ -3,6 +3,14 @@ package com.bfs.kotliniak.feature1
 class Feature2 {
 
 
+    fun tes33334t() {
+
+        val other = 3
+        val chage1 = 2
+        val ot111her = 3
+        val ch111a23ge1 = 2
+
+    }
 
 
     fun tes34t() {
