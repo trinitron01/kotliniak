e2f5062847474d9150c085696cc026129388f08c
Bartłomiej Sory <trinitron01@wp.pl>
2021-03-30 14:19:15 +0200

    Conflict2 (#16)
    feature 2

--- app/src/main/java/com/bfs/kotliniak/feature1/MasterChangeRenamedAgain.kt
+++ app/src/main/java/com/bfs/kotliniak/feature1/MasterChangeRenamedAgain.kt
@@ -11,20 +11,4 @@ class MasterChangeRenamedAgain {
         val other = 3
         val chage1 = 2
     }
-
-    fun te2st() {
-
-        val other = 3
-        val chage1 = 2
-    }
-
-
-
-    fun te22st() {
-
-        val other = 3
-        val chage1 = 2
-    }
-
-
 }
