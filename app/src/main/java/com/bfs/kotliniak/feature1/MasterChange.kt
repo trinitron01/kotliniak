5061d8815425c60b7ba7068bb047befec1045725
Bartłomiej Sory <bartlomiej.sory@intive.com>
2021-03-30 14:31:12 +0200

    master change

--- app/src/main/java/com/bfs/kotliniak/feature1/MasterChange.kt
+++ app/src/main/java/com/bfs/kotliniak/feature1/MasterChange.kt
@@ -2,5 +2,11 @@ package com.bfs.kotliniak.feature1
 
 class MasterChange {
 
+    fun masterBranchChange() {
 
+        val master1  = true
+        val master3  = true
+        val master4  = true
+        val master5  = true
+    }
 }
