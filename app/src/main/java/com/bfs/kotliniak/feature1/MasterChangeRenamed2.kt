86b823d315eccbf2e1abe595406b855e44a40c9b
Bartłomiej Sory <bartlomiej.sory@intive.com>
2021-03-30 14:22:20 +0200

    master CHANGE 3

---
+++ app/src/main/java/com/bfs/kotliniak/feature1/MasterChangeRenamed2.kt
@@ -0,0 +1,18 @@
+package com.bfs.kotliniak.feature1
+
+class MasterChangeRenamed2 {
+
+    fun masterChange1() {
+
+        val master = true
+        val masterB = false
+        val masterB2 = false
+    }
+
+
+    fun masterAdded() {
+
+     //todo
+    }
+
+}
